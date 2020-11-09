0accc474ab48d16f81721906cdc9385e62143a25
sabertooth-harsh <bawari.h@sabertoothtech.in>
2020-11-09 14:57:37 +0530

    Redux Form complete with Submit

--- components/main.js
+++ components/main.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Button } from 'react-native-elements';
+import { Input, Button, Card, Icon } from 'react-native-elements';
 import * as actionTypes from '../redux/actionTypes';
 import { connect } from 'react-redux';
 import { addUserInfo } from '../redux/actionCreators';
@@ -24,16 +24,65 @@ class Main extends Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            fName: '',
+            lName: '',
+            email: '',
+            pwd: '',
+            contact: ''
+        }
+    }
+
+    handleChange(e) {
+
     }
 
 
     render() {
-        console.log(this.props);
-        this.props.addUserInfo('harsh', 'bawari', 'harshbawari@gmail.com', 'harshbawari', '1234567890');
-        console.log(this.props);
         return (
             < View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>{Object.values(this.props)}</Text>
+                <Card containerStyle={{ width: 400 }}>
+                    <Card.Title>Enter your information</Card.Title>
+                    <Card.Divider />
+                    <View style={{ flexDirection: 'row' }}>
+                        <Input
+                            value={this.state.fName}
+                            containerStyle={{ width: 150 }}
+                            placeholder='First Name'
+                            onChangeText={(value) => this.setState({ fName: value })}
+                        />
+                        <Input
+                            value={this.state.lName}
+                            containerStyle={{ width: 150 }}
+                            placeholder='Last Name'
+                            onChangeText={(value) => this.setState({ lName: value })}
+                        />
+                    </View>
+                    <Input
+                        value={this.state.email}
+                        placeholder='Email'
+                        onChangeText={(value) => this.setState({ email: value })}
+                    />
+                    <Input
+                        value={this.state.pwd}
+                        placeholder='Password'
+                        onChangeText={(value) => this.setState({ pwd: value })}
+                    />
+                    <Input
+                        value={this.state.contact}
+                        placeholder='Contact'
+                        onChangeText={(value) => this.setState({ contact: value })}
+                    />
+                    <Card.Divider />
+                    <Button
+                        title='Submit'
+                        onPress={(event) => {
+                            this.props.addUserInfo(this.state.fName, 'bawari', 'harshu@gmail', 'harshu', '123456');
+                            console.log(this.props);
+                            this.setState({ fName: '', lName: '', email: '', pwd: '', contact: '' });
+                        }}
+                    />
+                </Card>
             </View >
         );
     }
